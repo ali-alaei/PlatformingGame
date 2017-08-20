96b524bc2f111a07c8621f5e00bda185a3bf5ca4
Hamed Safaei <hamedsafaei97@yahoo.com>
2017-08-20 13:20:37 +0430

    Main Character's movement implemented
    A sample project's files copied and the main purpose of this projects is the implemetation the main character's movement ...
    Main Character can move left and right and can jump...

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 10.0f;
+    public float maxVelocityChange = 10.0f;
+    public float jumpForce = 500f;
+    public float frictionCoefficient = 7f;
+
+    private Rigidbody2D rigidBody;
+    private float targetVelocity;
+    private float velocity;
+    private float velocityChange;
+    private bool facingRight = true;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.freezeRotation = true;
+    }
+    
+    void FixedUpdate()
+    {
+        targetVelocity = Input.GetAxis("Horizontal");
+        targetVelocity *= speed;
+        velocity = rigidBody.velocity.x;
+        velocityChange = (targetVelocity - velocity);
+        velocityChange = Mathf.Clamp(velocityChange, -maxVelocityChange, maxVelocityChange);
+        rigidBody.AddForce(new Vector2(velocityChange * frictionCoefficient, 0));
+
+        if (Input.GetButton("Jump"))
+        {
+            if (rigidBody.velocity.y == 0f)
+            {
+                rigidBody.AddForce(new Vector2(0, jumpForce));
+            }
+        }
+
+        if (targetVelocity > 0 && !facingRight)        
+            Flip();
+        else if (targetVelocity < 0 && facingRight)
+            Flip();
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+    
+}
