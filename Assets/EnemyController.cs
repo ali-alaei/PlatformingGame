96e8c2f2a96026dc11e405428cb5d8ac2f9fb2a3
Ibra <miladibra10@gmail.com>
2017-08-27 19:36:44 +0430

    Patrolling implemented
    Enemypatrolling implemented

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+	public GameObject startPoint;
+	public GameObject endPoint;
+	public float speed ;
+	private bool isInMiddle = false;
+	void Start()
+	{
+		if (transform.position.x >= startPoint.transform.position.x && transform.position.x <= endPoint.transform.position.x) {
+			isInMiddle = true;
+			flipRight ();
+			speed = Mathf.Abs (speed);
+		}
+		else if(transform.position.x < startPoint.transform.position.x)
+		{
+			flipRight ();
+			speed = Mathf.Abs (speed);
+		}
+		else if(transform.position.x > endPoint.transform.position.x)
+		{
+			flipLeft ();
+			speed = -1 * Mathf.Abs (speed);
+		}
+
+	}
+	void Update()
+	{
+		if (transform.position.x >= startPoint.transform.position.x && transform.position.x <= endPoint.transform.position.x) {
+			isInMiddle = true;
+		}
+		if (isInMiddle) {
+			if (transform.position.x < startPoint.transform.position.x) {
+				flipRight ();
+				speed = Mathf.Abs (speed);
+			} else if (transform.position.x > endPoint.transform.position.x) {
+				flipLeft ();
+				speed = -1 * Mathf.Abs (speed);
+			}
+		} 
+		transform.Translate (new Vector3 (speed, 0, 0) * Time.deltaTime);
+	}
+	void flipRight()
+	{
+		Vector3 scale = transform.localScale;
+		scale.x = 1;
+		transform.localScale = scale;
+	}
+	void flipLeft()
+	{
+		Vector3 scale = transform.localScale;
+		scale.x = -1;
+		transform.localScale = scale;
+	}
+}
