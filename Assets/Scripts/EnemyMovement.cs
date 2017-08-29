10eeab18148564a2c334ca200e9339775aafbfdf
Ibra <miladibra10@gmail.com>
2017-08-30 01:43:53 +0430

    Enemy Shooting & Moving with new structure implemented
    new enemy sprite added!

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	public float speed = 2.0f;
+	public GameObject startPoint;
+	public GameObject endPoint;
+
+	private Rigidbody2D rigidBody;
+	private bool isInMiddle = false;
+
+	void Awake()
+	{
+		rigidBody = GetComponent<Rigidbody2D>();
+		rigidBody.freezeRotation = true;
+	}
+
+	void Start()
+	{
+		if (transform.position.x >= startPoint.transform.position.x && transform.position.x <= endPoint.transform.position.x) {
+			isInMiddle = true;
+			flipRight ();
+			speed = Mathf.Abs (speed);
+		}
+		else if(transform.position.x < startPoint.transform.position.x)
+		{
+			flipRight ();
+			speed = Mathf.Abs (speed);
+		}
+		else if(transform.position.x > endPoint.transform.position.x)
+		{
+			flipLeft ();
+			speed = -1 * Mathf.Abs (speed);
+		}
+		rigidBody.velocity = new Vector2 (speed, 0);
+		//rigidBody.velocity.x = speed;
+	}
+	void FixedUpdate()
+	{
+		if (transform.position.x >= startPoint.transform.position.x && transform.position.x <= endPoint.transform.position.x) {
+			isInMiddle = true;
+		}
+		if (isInMiddle) {
+			if (transform.position.x < startPoint.transform.position.x) {
+				flipRight ();
+				speed = Mathf.Abs (speed);
+				rigidBody.velocity = new Vector2 (speed, 0);
+				//rigidBody.velocity.x = speed;
+			} else if (transform.position.x > endPoint.transform.position.x) {
+				flipLeft ();
+				speed = -1 * Mathf.Abs (speed);
+				rigidBody.velocity = new Vector2 (speed, 0);
+				//rigidBody.velocity.x = speed;
+			}
+		}
+
+		//transform.Translate (new Vector3 (speed, 0, 0) * Time.deltaTime);
+	}
+	void flipRight()
+	{
+		Vector3 scale = transform.localScale;
+		scale.x = -1*Mathf.Abs(scale.x);
+		transform.localScale = scale;
+	}
+	void flipLeft()
+	{
+		Vector3 scale = transform.localScale;
+		scale.x = Mathf.Abs(scale.x);
+		transform.localScale = scale;
+	}
+
+}
---
+++ Assets/Scripts/EnemyShooting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour {
+	[SerializeField]
+	private Transform firePoint;
+	[SerializeField]
+	private GameObject bullet;
+
+	private float shootDelay=1.5f;
+
+	private bool isShooting =true;
+
+	void Update () {
+		if (isShooting)
+		{
+			if (transform.localScale.x>0)
+			{
+				Instantiate(bullet, firePoint.position, Quaternion.identity);
+			}
+			if (transform.localScale.x<0)
+			{
+				Instantiate(bullet, firePoint.position, Quaternion.Euler(0, 0, 180));
+			}
+		}
+		StartCoroutine (wait (shootDelay));
+	}
+	IEnumerator wait(float seconds)
+	{
+		yield return new WaitForSeconds(seconds);
+	}
+}
