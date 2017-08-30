b579466cdb43ce658028e112cb83cf28a9dc08a6
Hamed Safaei <hamedsafaei97@yahoo.com>
2017-08-30 12:02:26 +0430

    Camera Vertical Movement
    camera horizontal movement modified and camera vertical movement is implemented...

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,22 +6,13 @@ public class CameraController : MonoBehaviour
 {
 
     public GameObject player;
-    public float horizontalEdgePosition;
 
-    private Rigidbody2D rigidbody2D;
-    private float playerAndCameraHorizontalDistance;
 	void Start ()
 	{
-	    rigidbody2D = GetComponent<Rigidbody2D>();
 	}
 	
 	void Update ()
 	{
-	    playerAndCameraHorizontalDistance = player.transform.position.x - transform.position.x;
-	    if (Mathf.Abs(playerAndCameraHorizontalDistance) > Mathf.Abs(horizontalEdgePosition))
-	    {
-            float distance = playerAndCameraHorizontalDistance - (playerAndCameraHorizontalDistance > 0 ? horizontalEdgePosition : -horizontalEdgePosition);
-            transform.position = new Vector3(transform.position.x + distance, transform.position.y, transform.position.z);
-	    }
-	}
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+    }
 }
