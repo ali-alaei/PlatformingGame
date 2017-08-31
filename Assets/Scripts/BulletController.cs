07d378c930ad15e27c0ebf7f4e797c4ce167f9c0
Ibra <miladibra10@gmail.com>
2017-08-31 20:05:16 +0430

    Moving of Bullets implemented

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+
+
+public class BulletController : MonoBehaviour 
+{ 
+	public float speed; 
+
+	private Rigidbody2D myRigidBody;
+	Time start ;
+
+	void Start()
+	{
+		myRigidBody = GetComponent<Rigidbody2D> ();
+		if (transform.localScale.x > 0) {
+			speed = -1 * Mathf.Abs (speed);
+		} else {
+			speed = Mathf.Abs (speed);
+		}
+		//myRigidBody.velocity = new Vector2 (speed, 0);
+	}
+	void Update()
+	{
+		transform.Translate (new Vector3 (speed, 0, 0) * Time.deltaTime);
+	}
+
+
+} 
--- Assets/Scripts/EnemyShooting.cs
+++ Assets/Scripts/EnemyShooting.cs
@@ -8,26 +8,27 @@ public class EnemyShooting : MonoBehaviour {
 	[SerializeField]
 	private GameObject bullet;
 
-	private float shootDelay=1.5f;
+	private float shootDelay=2f;
 
 	private bool isShooting =true;
 
 	void Update () {
 		if (isShooting)
 		{
-			if (transform.localScale.x>0)
-			{
-				Instantiate(bullet, firePoint.position, Quaternion.identity);
-			}
 			if (transform.localScale.x<0)
 			{
-				Instantiate(bullet, firePoint.position, Quaternion.Euler(0, 0, 180));
+				Vector2 firstscale = bullet.transform.localScale;
+				Vector2 scale = bullet.transform.localScale;
+				scale.x *= -1;
+				bullet.transform.localScale = scale;
+				Instantiate(bullet, firePoint.position, Quaternion.identity);
+				bullet.transform.localScale = firstscale;
+			}
+			else if (transform.localScale.x>0)
+			{
+				Instantiate (bullet, firePoint.position, Quaternion.identity );
 			}
 		}
-		StartCoroutine (wait (shootDelay));
 	}
-	IEnumerator wait(float seconds)
-	{
-		yield return new WaitForSeconds(seconds);
-	}
+
 }
