88a3ec35a3a2363fca730677b6bf65e7df9a3603
ASP761227 <aryan761227@gmail.com>
2017-09-01 08:01:05 -0700

    primary
    the primary version of platfrom.the player can jump on platforms and the map of game had moving platform and elevator and ladder and coins that player can pick up them and boxes that can move them.

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+
+	public Transform origin;
+	public Transform destination;
+	public float speed = 5f;
+
+	private Transform _myTransform;
+	private bool _switching;
+
+
+	// Use this for initialization
+	void Start () {
+		_myTransform = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_myTransform.position == origin.position) {
+			
+			_switching = true;
+		}
+
+
+
+		if (_switching == true) {
+		
+			_myTransform.position = Vector3.MoveTowards (_myTransform.position, destination.position, 5 * Time.deltaTime);
+
+		}
+			
+
+		if (_myTransform.position == destination.position) {
+
+			_switching = false;
+
+		}
+		if (_switching == false) {
+
+			_myTransform.position = Vector3.MoveTowards (_myTransform.position, origin.position, 5 * Time.deltaTime);
+
+			}
+
+		}
+}
+
