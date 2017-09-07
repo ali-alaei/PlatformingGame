78a54c987eabcf5e920bceae6655200a79edc998
aliak <aainsrm@gmail.com>
2017-09-07 17:20:52 +0430

    weapons sprites added , initial switch script created

---
+++ Assets/IntialPlatformerGame/Scripts/PlayerWeaponHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeaponHandler : MonoBehaviour {
+
+
+    public GameObject[] weapons; // push your prefabs
+
+    public int currentWeapon = 0;
+
+    private int nrWeapons;
+
+    void Start()
+    {
+
+        nrWeapons = weapons.Length;
+
+        SwitchWeapon(currentWeapon); // Set default gun
+
+    }
+
+    void Update()
+    {
+        for (int i = 1; i <= nrWeapons; i++)
+        {
+            if (Input.GetKeyDown("" + i))
+            {
+                currentWeapon = i - 1;
+
+                SwitchWeapon(currentWeapon);
+
+            }
+        }
+
+    }
+
+    void SwitchWeapon(int index)
+    {
+
+        for (int i = 0; i < nrWeapons; i++)
+        {
+            if (i == index)
+            {
+                weapons[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                weapons[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+}
