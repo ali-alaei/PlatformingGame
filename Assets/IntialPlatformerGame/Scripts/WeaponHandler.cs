a249139725e0e7ae01d2f6cc6bc2ead6ca46ae0c
aliak <aainsrm@gmail.com>
2017-09-08 09:22:53 +0430

    initial weapon switching implemented

---
+++ Assets/IntialPlatformerGame/Scripts/WeaponHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHandler : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject colt;
+
+	// Use this for initialization
+	void Start () {
+        colt.transform.parent = player.transform;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
