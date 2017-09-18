d83511be65f0b511a906114a4aec983d83f4a978
Hamed Safaei <hamedsafaei97@yahoo.com>
2017-09-18 17:56:26 +0430

    changes on player movment and camera

---
+++ Assets/Scripts/PlatformPlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformPlayerController : MonoBehaviour
+{
+
+
+    private PlayerMovement character;
+    private bool jump = false;
+
+    float hInput = 0;
+    float jInput = 0;
+
+    private void Awake()
+    {
+        character = GetComponent<PlayerMovement>();
+    }
+    // Update is called once per frame
+
+    void Update () {
+        print(jump);
+        if (!jump)
+        {
+            // Read the jump input in Update so button presses aren't missed.
+            jump = Input.GetButtonDown("Jump");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+#if UNITY_EDITOR
+        bool crouch = Input.GetKey(KeyCode.LeftControl);
+        float h = Input.GetAxis("Horizontal");
+        // Pass all parameters to the character control script.
+        character.Move(h, crouch, jump);
+        jump = false;
+#elif UNITY_ANDROID
+        character.Move(hInput, crouch, jump);
+        jump = false;
+#endif
+    }
+
+    public void StartMoving(float horizonalInput)
+    {
+        hInput = horizonalInput;
+    }
+
+    public void StartJumping()
+    {
+        jump = true;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,53 +7,74 @@ using UnityEngine.UI;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float speed = 10.0f;
-    public float maxVelocityChange = 10.0f;
-    public float jumpForce = 500f;
-    public float frictionCoefficient = 7; 
-    private Rigidbody2D rigidBody;
-    private float velocity;
-    private float velocityChange;
-    private bool facingRight = true;
+    [SerializeField] private float maxSpeed = 10f;
+    [SerializeField] private float jumpForce = 400f;
+    [Range(0, 1)] [SerializeField] private float crouchSpeed = .36f;
+    [SerializeField] private bool airControl = false;
+    [SerializeField] private LayerMask whatIsGround;
+
+    private Transform groundCheck;    // A position marking where to check if the player is grounded.
+    const float groundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+    private bool grounded;            // Whether or not the player is grounded.
+    private Transform ceilingCheck;   // A position marking where to check for ceilings
+    const float ceilingRadius = .01f;
+    [HideInInspector] public bool facingRight = true;
+    private Rigidbody2D rigidBody2d;
+
     float hInput = 0;
     float jInput = 0;
-
-    public Text countText;
-     
-
-    private int count = 5;
-   
-
-    void Start()
-    {
-        
-        
-    }
     void Awake()
     {
-        rigidBody = GetComponent<Rigidbody2D>();
-        rigidBody.freezeRotation = true;
+        groundCheck = transform.Find("GroundCheck");
+        ceilingCheck = transform.Find("CeilingCheck");
+    
+        rigidBody2d = GetComponent<Rigidbody2D>();
+        rigidBody2d.freezeRotation = true;
     }
-    private void Update()
-    {
-        
-        count = Convert.ToInt32(countText.text); 
-    }
-
     void FixedUpdate()
     {
-#if UNITY_EDITOR
-        float targetVelocity = Input.GetAxis("Horizontal");
-        Move(targetVelocity);
-        if (Input.GetButton("Jump"))
+        grounded = false;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, groundedRadius, whatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
         {
-            float jumpPower = jumpForce;
-            jump(jumpPower);
+            if (colliders[i].gameObject != gameObject)
+                grounded = true;
         }
-#elif UNITY_ANDROID
-        Move(hInput);
-        jump(jInput);
-#endif
+    }
+    public void Move(float move, bool crouch, bool jump)
+    {
+        /**if (!crouch && Physics2D.OverlapCircle(ceilingCheck.position, ceilingRadius, whatIsGround))
+        {
+            // If the character has a ceiling preventing them from standing up, keep them crouching                        
+            crouch = true;
+        }**/
+
+        if (grounded || airControl)
+        {
+            // Reduce the speed if crouching by the crouchSpeed multiplier
+            move = (crouch ? move*crouchSpeed : move);
+        }
+
+        rigidBody2d.velocity = new Vector2(move * maxSpeed, rigidBody2d.velocity.y);
+
+        // If the input is moving the player right and the player is facing left...
+        if (move > 0 && !facingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (move < 0 && facingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+
+        if (grounded && jump)
+        {
+            // Add a vertical force to the player.
+            grounded = false;            
+            rigidBody2d.AddForce(new Vector2(0f, jumpForce));        }
     }
 
     void Flip()
@@ -73,49 +94,4 @@ public class PlayerMovement : MonoBehaviour
     {
         jInput = jumpInput;
     }
-
-    public void Move(float targetVelocity)
-    {
-        targetVelocity *= speed;
-        velocity = rigidBody.velocity.x;
-        velocityChange = (targetVelocity - velocity);
-        velocityChange = Mathf.Clamp(velocityChange, -maxVelocityChange, maxVelocityChange);
-        rigidBody.AddForce(new Vector2(velocityChange * frictionCoefficient, 0));
-        if (targetVelocity > 0 && !facingRight)
-        {
-            Flip();
-        }
-        else if (targetVelocity < 0 && facingRight)
-        {
-            Flip();
-        } 
-    }
-
-    public void jump(float jumpForce)
-    {
-        if (rigidBody.velocity.y == 0f)
-        {
-            rigidBody.AddForce(new Vector2(0, jumpForce));
-        }
-    }
-
-
-    void OnCollisionEnter2D(Collision2D coll)
-    {
-        if (coll.gameObject.CompareTag("Elevator"))
-        {
-            if (transform.position.y > coll.transform.position.y + 1)
-                transform.SetParent(coll.transform);
-        }
-    }
-
-    void OnCollisionExit2D(Collision2D coll)
-    {
-        if (coll.gameObject.CompareTag("Elevator"))
-        {
-            transform.SetParent(null);
-        }
-    }
-
-
 }
