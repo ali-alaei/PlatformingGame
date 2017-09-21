c69861a93154cf91565d3110b4223e25b3bca7de
ASP761227 <aryan761227@gmail.com>
2017-09-21 13:22:42 -0700

    EventHadler on HealthBox
    in this part player has a variable named Health and HealthBox via Events can Informed player from increasing his health.

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+    //public delegate void ClickAction();
+    //public static event ClickAction Destroying;
+
+    
+    public Image image;
+    private float maxHealth = 100f;
+    private float curHealth = 0f;
+
+    void Start () {
+
+        curHealth = maxHealth;
+        
+    }
+     
+    void Update () {
+        curHealth = image.fillAmount * 100;
+    }
+
+    private void OnEnable()
+    {
+        HealthBox.HealthUp += increasehealth;
+    }
+
+    private void OnDisable()
+    {
+        HealthBox.HealthUp -= increasehealth;
+    }
+
+
+    float increasehealth()
+    {
+        if (curHealth < 100)
+        {
+            curHealth += 20f;
+            float calchealth = curHealth / maxHealth;
+            setHealth(calchealth);
+            //Destroying();
+        }
+        return image.fillAmount;
+    }
+
+    void setHealth(float myHealth)
+    {
+        image.fillAmount = myHealth;
+    }
+
+}
